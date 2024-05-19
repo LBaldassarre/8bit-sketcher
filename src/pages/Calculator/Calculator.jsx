e457e691638ddfcfaeb5dc552bd6645aae63861a
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-05-19 11:23:11 +0200

    Add sliding left pane

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -5,8 +5,7 @@ function Calculator() {
     return (
       <div>
         <Nav />
-        <main>
-          <div className='function_selector'>
+        <div className='function_selector'>
             <ul className='function_list'>
               <li className='function'>Function 1</li>
               <li className='function'>Function 2</li>
@@ -20,26 +19,25 @@ function Calculator() {
               <li className='function'>Function 10</li>
             </ul>
           </div>
-          <div className='calculation'>
-            <h2 className='calculation_title'>Function 1</h2>
-            <h3 className='calculation_description'>Description</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.</p>
-            <ul className='variable_list'>
-              <li className='variable'>a:</li>
-              <li className='variable'>c:</li>
-              <li className='variable'>c:</li>
-              <li className='variable'>b:</li>
-              <li className='variable'>d:</li>
-              <li className='variable'>e:</li>
-              <li className='variable'>f:</li>
-            </ul>
-            <div className='buttons'>
-              <button className='btn'>Reset</button>
-              <button className='btn'>Calculate</button>
-            </div>
-            <div className='result'>
-              Result: 100.004
-            </div>
+        <main>
+          <h2 className='calculation_title'>Function 1</h2>
+          <h3 className='calculation_description'>Description</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.</p>
+          <ul className='variable_list'>
+            <li className='variable'>a:</li>
+            <li className='variable'>c:</li>
+            <li className='variable'>c:</li>
+            <li className='variable'>b:</li>
+            <li className='variable'>d:</li>
+            <li className='variable'>e:</li>
+            <li className='variable'>f:</li>
+          </ul>
+          <div className='buttons'>
+            <button className='btn'>Reset</button>
+            <button className='btn'>Calculate</button>
+          </div>
+          <div className='result'>
+            Result: 100.004
           </div>
         </main>
       </div>
