38cec43dbbf225bcb40592c52a14bf042bcfec6d
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-06-15 21:42:38 +0200

    Add function list json based functionality

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import parse from 'html-react-parser';
+
 import './Calculator.css'
 import Nav from '../../components/Nav/Nav';
 
@@ -7,21 +9,132 @@ function Calculator() {
 
   const [funcSelOpen, setFuncSelOpen] = useState(false);
 
+  const functions = [
+    {
+      name: 'Function 1',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: true
+    },
+    {
+      name: 'Function 2',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 3',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 4',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 5',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 6',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 7',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 8',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 9',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    },
+    {
+      name: 'Function 10',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.',
+      variables: ['a', 'b', 'c', 'd'],
+      calculation: 'a+b+c+d',
+      expression: 'a + b + c + d',
+      selected: false
+    }
+  ]
+
+  function createFunctionList (functions) {
+    let list_html = "<li className='function'>" + functions[0].name + "</li>";
+
+    for (let i = 1; i < functions.length; i++) {
+      list_html += "\n<li className='function'>" + functions[i].name + "</li>";
+    }
+
+    return list_html;
+  }
+
+  function functionSelected(functions){
+    let functionSelected;
+
+    for (let i = 0; i < functions.length; i++) {
+      if (functions[i].selected) {
+        functionSelected = functions[i];
+        return functionSelected;
+      }
+    }
+  }
+
+  const selectedFunction = functionSelected(functions);
+
+  function functionSelectedVariablesHTML (selectedFunction) {
+    let variables = "<li className='variable'><span>" + selectedFunction.variables[0] + ":</span> <input type='text' /></li>";
+
+    for (let i = 1; i < selectedFunction.variables.length; i++) {
+      variables += "\n<li className='variable'><span>" + selectedFunction.variables[i] + ":</span> <input type='text' /></li>";
+    }
+
+    return variables;
+  }
+
+  const selectedFunctionVars = functionSelectedVariablesHTML(selectedFunction);
+
+  const function_list_html = createFunctionList(functions);
+
     return (
       <div>
         <Nav />
         <div className= {funcSelOpen ? 'function_selector fs_open' : 'function_selector'}>
             <ul className='function_list'>
-              <li className='function'>Function 1</li>
-              <li className='function'>Function 2</li>
-              <li className='function'>Function 3</li>
-              <li className='function'>Function 4</li>
-              <li className='function'>Function 5</li>
-              <li className='function'>Function 6</li>
-              <li className='function'>Function 7</li>
-              <li className='function'>Function 8</li>
-              <li className='function'>Function 9</li>
-              <li className='function'>Function 10</li>
+              {parse(function_list_html)}
             </ul>
             <div>
             <span onClick={() => setFuncSelOpen(!funcSelOpen)}><i className = {funcSelOpen ? "uil uil-times pane_btn_close" : "uil uil-times pane_btn_close display_none"}></i></span>
@@ -29,16 +142,11 @@ function Calculator() {
         </div>
         <span onClick={() => setFuncSelOpen(!funcSelOpen)}><i className= {!funcSelOpen ? "uil uil-angle-right pane_btn_open" : "uil uil-angle-right pane_btn_open display_none"}></i></span>
         <main className='section container'>
-          <h2 className='calculation_title'>Function 1</h2>
+          <h2 className='calculation_title'>{selectedFunction.name}</h2>
           <h3 className='calculation_description'>Description</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quas error veritatis dolorem iure. Exercitationem asperiores reprehenderit, sunt numquam et explicabo. Illo, corporis reprehenderit fugit incidunt voluptatum eveniet possimus. Nostrum.</p>
+          <p>{selectedFunction.description}.</p>
           <ul className='variable_list container'>
-            <li className='variable'><span>a:</span> <input type="text" /></li>
-            <li className='variable'><span>b:</span> <input type="text" /></li>
-            <li className='variable'><span>c:</span> <input type="text" /></li>
-            <li className='variable'><span>b:</span> <input type="text" /></li>
-            <li className='variable'><span>d:</span> <input type="text" /></li>
-            <li className='variable'><span>e:</span> <input type="text" /></li>
+            {parse(selectedFunctionVars)}
           </ul>
           <div className='buttons'>
             <button className='button'>Reset</button>
